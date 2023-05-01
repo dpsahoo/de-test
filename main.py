0cfb264ce495835256a891585bfd39ec73349b5a
Durga Sahoo <dpsahoo@gmail.com>
2023-05-01 11:39:14 +1000

    Added explode function

--- main.py
+++ main.py
@@ -17,9 +17,27 @@ def load_data_and_validate(spark):
     df = spark.read.json(input_files)
     df.printSchema()
     df.count()
+    return df
+
+def create_exploded_df(orig_df):
+    '''
+    # Create an exploded/expanded dataframe to perform further queries. 
+    # Needed to normalise 'jobHistory' array column 
+    '''
+    # explode jobHistory array column
+    df_exploded = df.selectExpr("id", "profile.firstName", "profile.lastName", "explode(profile.jobHistory) as jobHistory")
+    return df_exploded
+
+
 
 
 if __name__ == '__main__':
+    # Base functions
     spark_sess = create_session()
-    load_data_and_validate(spark_sess)
-    
+    df = load_data_and_validate(spark_sess)
+    df_exploded = create_exploded_df(df)
+
+    # Business queries
+
+
+
